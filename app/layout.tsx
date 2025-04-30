12061a997a751246c083efe34883b8bab6d7ac4e
Naman Saini <namansaini464@gmail.com>
2025-04-30 12:26:57 +0530

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import Navbar from "@/components/navbar";
 
@@ -28,8 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased text-white bg-zinc-900`}
       >
-        <Navbar/>
+        <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
