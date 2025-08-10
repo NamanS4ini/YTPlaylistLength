30a64c3a7f30e3f9edb724a5c82e8c55002ba8d3
Naman Saini <namansaini464@gmail.com>
2025-08-10 19:31:53 +0530

    Added Vercel speed Insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import Navbar from "@/components/navbar";
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
