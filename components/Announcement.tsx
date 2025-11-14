d2f489083bbbc83069ab656b7039d476802f94d3
Naman Saini <namansaini464@gmail.com>
2025-11-14 19:14:45 +0530

    Major fixed to playlist range and announcement

--- components/Announcement.tsx
+++ components/Announcement.tsx
@@ -14,7 +14,7 @@ const Announcement = () => {
         // Add more announcements here
         {
             id: 2,
-            message: `The website has moved to <a class="text-blue-400 underline hover:text-blue-300" href="https://ytpla.in">ytpla.in</a>. Please transfer your saved playlists by December 1st, 2025.`,
+            message: `The website has moved to <a class="text-blue-400 underline hover:text-blue-300" href="https://ytpla.in">ytpla.in</a>. Please transfer your saved playlists by November 20th, 2025.`,
             enabled: true,
         },
     ];
